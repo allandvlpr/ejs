37aff823adc2de122c4b76692950eb0ed974739f
allan <hebert.allan@outlook.com>
2025-08-04 09:01:52 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+app.set("view engine", "ejs")
+app.set("views", "./views")
+
+app.get("/", (request, response)=>{
+	response.render("index", {teste:"Garrafa Aggile"})
+})
+
+
+app.listen(3000, ()=>console.log("servidor ativo"))
